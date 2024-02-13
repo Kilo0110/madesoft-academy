d8cb3fad1194329a398f0faa4601b5def048098c
KamCy Ahaka <kamcyahaka@gmail.com>
2024-02-13 10:31:02 +0100

    feat(app.vue, nuxt.config.ts): add page transition logic

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
 <template>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+
+.page-enter-from {
+  transform: translateX(250px);
+  opacity: 0;
+}
+
+.page-leave-to {
+  transform: translateX(-250px);
+  opacity: 0;
+}
+</style>
