094db82e2fddf5cf8096b3bcaf06354a87884171
KamCy Ahaka <kamcyahaka@gmail.com>
2024-02-13 14:30:49 +0100

    style: update styling of checkbox

--- components/CustomCheckbox.vue
+++ components/CustomCheckbox.vue
@@ -6,7 +6,10 @@
       :id="props.id"
       class="w-5 h-5 text-theme-checkbox-clr bg-gray-100 border-gray-300 rounded-lg focus:ring-theme-fill-clr focus:ring-2"
     >
-    <label :for="props.id">{{ props.label }}</label>
+    <label
+      :for="props.id"
+      class="text-theme-icon-clr"
+    >{{ props.label }}</label>
   </div>
 </template>
 
