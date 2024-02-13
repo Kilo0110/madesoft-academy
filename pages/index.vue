802ca2758cc02a49e79990d2c41a3648ccedf7c1
KamCy Ahaka <kamcyahaka@gmail.com>
2024-02-13 10:34:59 +0100

    feat(index.vue): update index page view
    Index page now shows call to actions for login and register actions

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  Index Page
+  <main class="main min-h-screen flex flex-col justify-center gap-5">
+    <p class="welcome-text text-center text-2xl"><span class="text-4xl block font-bold">Hi there!</span> It's nice to
+      see you!</p>
+    <div class="btn-container flex flex-col items-center justify-center gap-5 md:flex-row">
+      <CallToAction
+        text="Log in"
+        @click="redirectToSignIn"
+        :uncolored="true"
+        class="px-6 w-full max-w-xs py-3 md:py-4"
+      />
+      <CallToAction
+        text="Create new account"
+        class="px-6 w-full max-w-xs py-3  md:py-4"
+        @click="redirectToSignUp"
+      />
+    </div>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: 'MadeSoft Academy Technologies || Log In / Sign Up'
+})
 
-<style lang="scss" scoped>
+const redirectToSignIn = () => {
+  return navigateTo('/auth/login')
+}
+const redirectToSignUp = () => {
+  return navigateTo('/auth/signup')
+}
+</script>
+
+<style scoped lang="scss">
 </style>
