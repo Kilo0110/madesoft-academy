2069034e11631fadde4a67851eed02af6709d3cc
KamCy Ahaka <kamcyahaka@gmail.com>
2024-02-13 10:33:36 +0100

    feat(CallToAction.vue): add styles for uncolored CTA

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,6 +1,7 @@
 <template>
   <button
-    class="call-to-action-btn border w-fit px-5 py-3 font-bold text-sm bg-theme-fill-clr text-white outline-none rounded-xl focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-theme-checkbox-clr"
+    class="call-to-action-btn border w-fit px-5 py-3 font-bold text-sm outline-none rounded-xl focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-theme-checkbox-clr"
+    :class="props.uncolored ? 'bg-transparent text-theme-text-clr border-2 border-theme-checkbox-clr' : 'bg-theme-fill-clr text-white'"
   >{{
     props.text
   }}</button>
@@ -9,6 +10,7 @@
 <script setup lang="ts">
 const props = defineProps<{
   text: string
+  uncolored?: boolean
 }>()
 </script>
 
